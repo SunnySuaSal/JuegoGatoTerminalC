de9c0037019d7fe4ed41df24b9d11e789904ed0f
[Suarez Saldana] <[yo_soyalegre@hotmail.com]>
2024-03-07 21:26:35 -0600

    Refactoriza el codigo en funciones inicializa e imprime

--- main.c
+++ main.c
@@ -1,11 +1,24 @@
 #include<stdio.h>
+
+void inicializaTablero(int tablero[3][3]);
+void imprimeTablero(int tablero[3][3]);
+
 int main(){
 	int tablero[3][3];
+	inicializaTablero(tablero);
+	imprimeTablero(tablero);
+	return 0;
+}
+
+void inicializaTablero(int tablero[3][3]){
 	for(int i=0; i<3; i++){
 		for(int j=0; j<3; j++){
 			tablero[i][j]=0;
 		}
 	}
+}
+
+void imprimeTablero(int tablero[3][3]){
 	for(int i=0; i<3; i++){
 		for(int j=0; j<3; j++){
 			printf("%d ", tablero[i][j]);
@@ -13,6 +26,6 @@ int main(){
 		printf("\n");
 	}
 
-
-	return 0;
 }
+
+
